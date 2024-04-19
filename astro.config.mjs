b1ec9453b0263aac7596695f7e95ec91e41475a8
Christopher Newton <christopher.k.newton@me.com>
2024-04-19 10:44:25 -0700

    Introduce PostCSS for media queries and nesting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://chrstphrknwtn.com',
   build: {
-    inlineStylesheets: 'always'
+    inlineStylesheets: 'never'
   }
 })
