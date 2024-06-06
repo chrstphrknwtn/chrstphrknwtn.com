f622edd8e20b3ead1c1b0b85dea5833d3077521a
Christopher Newton <christopher.k.newton@me.com>
2024-06-06 14:58:15 -0700

    add a few books

--- src/pages/bookshelf/index.tsx
+++ src/pages/bookshelf/index.tsx
@@ -23,11 +23,25 @@ const booklist = [
     year: 1967
   },
   {
+    href: 'https://www.collectiveinkbooks.com/zer0-books/our-books/capitalist-realism-new-edition?q=zer0-books/our-books/capitalist-realism',
+    coverImgSrc: '/bookshelf/capitalist-realism-mark-fisher.jpg',
+    title: 'Capitalist Realism',
+    author: 'Mark Fisher',
+    year: '2009 (2022)'
+  },
+  {
     href: 'https://www.oma.com/publications/delirious-new-york',
     coverImgSrc: '/bookshelf/delirious-new-york-rem-koolhaas.jpg',
     title: 'Delirious New York: A Retroactive Manifesto for Manhattan',
     author: 'Rem Koolhaas',
     year: 1978
+  },
+  {
+    href: 'https://archive.org/details/rolandbarthesannettelaversmythologieszlib.org',
+    coverImgSrc: '/bookshelf/mythologies-roland-barthes.jpg',
+    title: 'Mythologies',
+    author: 'Roland Barthes',
+    year: 1957
   },
   {
     href: 'https://www.hup.harvard.edu/books/9780674627512',
@@ -36,6 +50,13 @@ const booklist = [
     title: 'Notes on the Synthesis of Form',
     author: 'Christopher Alexander',
     year: 1964
+  },
+  {
+    href: 'https://archive.org/details/obeygiantlifeini0000poyn_g1v3',
+    coverImgSrc: '/bookshelf/obey-the-giant-rick-poynor.jpg',
+    title: 'Obey the Giant: Life in the Image World',
+    author: 'Rick Poynor',
+    year: 2007
   }
 ]
 
@@ -58,9 +79,7 @@ const Page = () => (
                     </figure>
                     <div>
                       <h3>{book.title}</h3>
-                      <p>
-                        {book.author} — {book.year}
-                      </p>
+                      <p>{book.author}</p>
                     </div>
                   </div>
                 </ExtLink>
