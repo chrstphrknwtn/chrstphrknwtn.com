2cbc6acf4b0074e38ded9747128706161d72b83d
Christopher Newton <christopher.k.newton@me.com>
2024-06-07 09:55:38 -0700

    fix layout shift

--- src/pages/bookshelf/index.tsx
+++ src/pages/bookshelf/index.tsx
@@ -10,50 +10,77 @@ import './bookshelf.css'
 const booklist = [
   {
     href: 'https://www.versobooks.com/products/1496-non-places',
-    coverImgSrc: '/bookshelf/non-places-marc-auge.jpg',
+    coverImg: {
+      src: '/bookshelf/non-places-marc-auge.jpg',
+      w: 192,
+      h: 281
+    },
     title: 'Non-Places: An Introduction to Supermodernity',
     author: 'Marc Augé',
     year: '2009'
   },
   {
     href: 'https://archive.org/details/mansrageforchaos0000peck',
-    coverImgSrc: '/bookshelf/mans-rage-for-chaos-morse-peckham.jpg',
+    coverImg: {
+      src: '/bookshelf/mans-rage-for-chaos-morse-peckham.jpg',
+      w: 192,
+      h: 276
+    },
     title: 'Man’s Rage for Chaos: Biology, Behavior, and the Arts',
     author: 'Morse Peckham',
     year: 1967
   },
   {
     href: 'https://www.collectiveinkbooks.com/zer0-books/our-books/capitalist-realism-new-edition?q=zer0-books/our-books/capitalist-realism',
-    coverImgSrc: '/bookshelf/capitalist-realism-mark-fisher.jpg',
+    coverImg: {
+      src: '/bookshelf/capitalist-realism-mark-fisher.jpg',
+      w: 192,
+      h: 296
+    },
     title: 'Capitalist Realism',
     author: 'Mark Fisher',
     year: '2009 (2022)'
   },
   {
     href: 'https://www.oma.com/publications/delirious-new-york',
-    coverImgSrc: '/bookshelf/delirious-new-york-rem-koolhaas.jpg',
+    coverImg: {
+      src: '/bookshelf/delirious-new-york-rem-koolhaas.jpg',
+      w: 192,
+      h: 274
+    },
     title: 'Delirious New York: A Retroactive Manifesto for Manhattan',
     author: 'Rem Koolhaas',
     year: 1978
   },
   {
     href: 'https://archive.org/details/rolandbarthesannettelaversmythologieszlib.org',
-    coverImgSrc: '/bookshelf/mythologies-roland-barthes.jpg',
+    coverImg: {
+      src: '/bookshelf/mythologies-roland-barthes.jpg',
+      w: 192,
+      h: 289
+    },
     title: 'Mythologies',
     author: 'Roland Barthes',
     year: 1957
   },
   {
     href: 'https://www.hup.harvard.edu/books/9780674627512',
-    coverImgSrc:
-      '/bookshelf/notes-on-the-systhesis-of-form-christopher-alexander.jpg',
+    coverImg: {
+      src: '/bookshelf/notes-on-the-systhesis-of-form-christopher-alexander.jpg',
+      w: 192,
+      h: 290
+    },
     title: 'Notes on the Synthesis of Form',
     author: 'Christopher Alexander',
     year: 1964
   },
   {
     href: 'https://archive.org/details/obeygiantlifeini0000poyn_g1v3',
-    coverImgSrc: '/bookshelf/obey-the-giant-rick-poynor.jpg',
+    coverImg: {
+      src: '/bookshelf/obey-the-giant-rick-poynor.jpg',
+      w: 192,
+      h: 259
+    },
     title: 'Obey the Giant: Life in the Image World',
     author: 'Rick Poynor',
     year: 2007
@@ -73,7 +100,9 @@ const Page = () => (
                   <div className="bookshelf-item">
                     <figure className="book-cover">
                       <img
-                        src={book.coverImgSrc}
+                        width={book.coverImg.w}
+                        height={book.coverImg.h}
+                        src={book.coverImg.src}
                         alt={`${book.title}, ${book.author}`}
                       />
                     </figure>
